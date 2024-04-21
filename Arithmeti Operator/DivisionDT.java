c21449c7b71952313a03bad0d4516d6dbcaae904
archanamalge <157685851+archanamalge@users.noreply.github.com>
2024-04-21 12:21:40 +0530

    These operators involve the mathematical operators

---
+++ Arithmeti Operator/DivisionDT.java
@@ -0,0 +1,46 @@
+class DivisionDT
+{
+	public static void main(String[] args)
+	{
+		byte b1=10;
+		byte b2=20;
+		int b3=b1/b2;
+		System.out.println(b3);
+
+		short sh1=2;
+		short sh2=3;
+		int sh3=sh1/sh2;
+		System.out.println(sh3);
+
+		char c1='A';
+		char c2='b';
+		int c3=c1/c2;
+		System.out.println(c3);
+
+		float f1=22.33f;
+		float f2=33.22f;
+		float f3=f1/f2;
+		System.out.println(f3);
+
+		double d1= 12.45;
+		double d2=39.01;
+		double d3=d1/d2;
+		System.out.println(d3);
+
+		int i1=5;
+		int i2=5;
+		int i3=i1/i2;
+		System.out.println(i3);
+
+		/*String s1="Java ";
+		String s2="Programming";
+		String s3=s1/s2;
+		System.out.println(s3);*/
+
+		long l1=4725383920l;
+		long l2=5342739037l;
+		long l3=l1/l2;
+		System.out.println(l3);
+
+	}
+}
